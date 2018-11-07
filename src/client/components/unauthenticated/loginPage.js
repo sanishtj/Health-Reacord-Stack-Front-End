a69f71a79d8d62f9f42605b98c852d838399279d
Sanish Joseph <sanyjose85@gmail.com>
2018-11-07 07:50:37 +0400

    correct UI for login page continuation

--- src/client/components/unauthenticated/loginPage.js
+++ src/client/components/unauthenticated/loginPage.js
@@ -38,7 +38,7 @@ class loginPage extends React.Component {
           </div>
           <div className="row">
             <div className="col">
-              <button type="button" className="btn btn-default">
+              <button type="button" className="btn btn-default header_title">
                 {'HOW IT WORKS'}
               </button>
             </div>
@@ -46,57 +46,91 @@ class loginPage extends React.Component {
         </div>
         <div id="middleframe" className="whitecontainercolor">
           <div className="row">
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-20">
-              <img
-                src="/images/FamilyHealth.png"
-                alt="add family members"
-                height="200px"
-                width="350px"
-                className=""
-              />
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+              <div className="py-3 px-2">
+                <img
+                  src="/images/FamilyHealth.png"
+                  alt="add family members"
+                  className="img-fluid rounded"
+                />
+              </div>
             </div>
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-20">
-              <h3>Manage your family members</h3>
-              <p>
-                {'You can add your family members to track health related data of'}
-                {'each members. We have restricted the maximum number of members'}
-                {'as five to restrict commercial uses. You are allowed to add, update, and remove your family members.'}
-              </p>
-              <button type="button" className="btn btn-default">
-                {'view more >'}
-              </button>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 order-2 order-sm-2 order-md-1 order-lg-1 order-xl-1 mb-20">
-              <h3>Manage medical history</h3>
-              <p>
-                {'Each member you add can have multiple medical histories.'}
-                {'You can manage medicines taken,hospitals visted, doctor details, bills,'}
-                {'insurence and most importanly MEDICAL RECORDS along with basic informations like initial symptopns'}
-              </p>
-            </div>
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 order-1 order-sm-1 order-md-2 order-lg-2 order-xl-2 mb-20">
-              <img
-                src="http://via.placeholder.com/350x200"
-                alt="Manage Medical History"
-              />
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+              <div className="py-3 px-2">
+                <h3>Manage your family members</h3>
+                <p>
+                  {
+                    'You can add your family members to track health related data of'
+                  }
+                  {
+                    'each members. We have restricted the maximum number of members'
+                  }
+                  {
+                    'as five to restrict commercial uses. You are allowed to add, update, and remove your family members.'
+                  }
+                </p>
+                <button type="button" className="btn btn-custom-1 d-none d-md-block d-lg-block">
+                  {'view more >'}
+                </button>
+              </div>
             </div>
           </div>
           <div className="row">
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-20">
-              <img
-                src="http://via.placeholder.com/350x200"
-                alt="Share any medical history"
-              />
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 order-2 order-sm-2 order-md-1 order-lg-1 order-xl-1">
+              <div className="py-3 px-2">
+                <h3>Manage medical history</h3>
+                <p>
+                  {'Each member you add can have multiple medical histories.'}
+                  {
+                    'You can manage medicines taken,hospitals visted, doctor details, bills,'
+                  }
+                  {
+                    'insurence and most importanly MEDICAL RECORDS along with basic informations like initial symptopns'
+                  }
+                </p>
+                <button type="button" className="btn btn-custom-1 d-none d-md-block d-lg-block">
+                  {'view more >'}
+                </button>
+              </div>
             </div>
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-20">
-              <h3>Share medical history</h3>
-              <p>
-                {'You can easily share any member\'s medical history you stored to doctors or hospital with a few clicks.'}
-                {'Your medical records will be attached in the email before sending emails.'}
-                {'You are allowed to edit history or add more notes in the mail before you mail.'}
-              </p>
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 order-1 order-sm-1 order-md-2 order-lg-2 order-xl-2">
+              <div className="py-3 px-2">
+                <img
+                  src="/images/ManageMedicalHistory.png"
+                  alt="add family members"
+                  className="img-fluid rounded"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+              <div className="py-3 px-2">
+                <img
+                  src="/images/ShareMedicalData.png"
+                  alt="add family members"
+                  className="img-fluid rounded"
+                />
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+              <div className="py-3 px-2">
+                <h3>Share medical history</h3>
+                <p>
+                  {
+                    "You can easily share any member's medical history you stored to doctors or hospital with a few clicks."
+                  }
+                  {
+                    'Your medical records will be attached in the email before sending emails.'
+                  }
+                  {
+                    'You are allowed to edit history or add more notes in the mail before you mail.'
+                  }
+                </p>
+                <button type="button" className="btn btn-custom-1 d-none d-md-block d-lg-block">
+                  {'view more >'}
+                </button>
+              </div>
             </div>
           </div>
         </div>
